71b7d5003125c896b1fc863ea1524f9939d1365c
Igor Santos <igor.santos@vilt-group.com>
2019-03-11 21:48:51 -0300

    dev

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,17 @@
 $(document).ready(function (){
 
+  $('.vkf-modal-open').click(function(e) {
+	  e.preventDefault();
+	  var modalOpen = $(this).attr('href');
+	  $('.vkf-modal').hide();
+	  $(modalOpen).fadeIn('fast');
+	  $('html, body').css('overflow','hidden');
+	});
 
+	$('.vkf-modal__close').click(function(e) {
+	  e.preventDefault();
+	  $('html, body').css('overflow','');
+	  $('.vkf-modal').fadeOut('fast');
+	});
 
 });
